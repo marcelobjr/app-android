d11e7d7dbb65f9a79e910df077cb4f6299354f75
Marcelo Barros <marcelobjr@gmail.com>
2017-08-08 20:21:25 -0300

    first commit

---
+++ app/src/main/java/com/example/marcelo/aulaandroid/HelloWord.java
@@ -0,0 +1,13 @@
+package com.example.marcelo.aulaandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_word);
+    }
+}
